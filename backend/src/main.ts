76d63a428aad49016a3662d79b99180689500924
EC2 Default User <ec2-user@ip-172-31-25-191.eu-north-1.compute.internal>
2026-08-19 22:30:21 +0000

    Add helmet security headers

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,11 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
 
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
